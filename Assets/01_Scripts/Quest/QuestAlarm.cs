eaa6479f924501292c16a90726b52cab426a3c39
HuiJin son <54790406+gkgk2412@users.noreply.github.com>
2021-08-18 22:29:06 +0900

    .
    .

---
+++ Assets/01_Scripts/Quest/QuestAlarm.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestAlarm : MonoBehaviour
+{
+    [SerializeField] private GameObject _AlarmPrefab;
+    [SerializeField] private Transform _AlarmParent;
+
+    public void GetQuestAlarm(Quest quest)
+    {
+        GameObject Alarm = Instantiate(_AlarmPrefab, _AlarmParent);
+
+        string QuestAlr = "퀘스트 " + quest.MyTitle + " 수락!";
+
+        Alarm.transform.GetChild(0).GetComponent<Text>().text = QuestAlr;
+    }
+}
--- Assets/01_Scripts/Quest/QuestGiver.cs
+++ Assets/01_Scripts/Quest/QuestGiver.cs
@@ -11,27 +11,33 @@ public class QuestGiver : MonoBehaviour
     [SerializeField]
     private QuestLog tmpLog;
 
+    [SerializeField] private QuestAlarm _questAlarm;
+
     //Main Quest를 받은 상태이면 
     public void MainQuestEvent()
-    {      
+    {        
+        _questAlarm.GetQuestAlarm(quests[0]);
         tmpLog.AcceptQuest(quests[0]);
     }
 
     //버섯돌이 Quest
     public void SubQuest01Event()
     {
+        _questAlarm.GetQuestAlarm(quests[1]);
         tmpLog.AcceptQuest(quests[1]);
     }
 
     //무 몬스터 quest
     public void SubQuest02Event()
     {
+        _questAlarm.GetQuestAlarm(quests[2]);
         tmpLog.AcceptQuest(quests[2]);
     }
 
     //수정골렘 quest
     public void SubQuest03Event()
     {
+        _questAlarm.GetQuestAlarm(quests[3]);
         tmpLog.AcceptQuest(quests[3]);
     }
 }
